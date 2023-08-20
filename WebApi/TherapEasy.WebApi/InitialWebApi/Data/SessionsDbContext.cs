7676d77cade6d784d218ab132d8ca5e3d4aa686a
Igor Corrêa <125756540+IRGO-CO@users.noreply.github.com>
2023-08-20 02:10:38 -0300

    mais

--- WebApi/TherapEasy.WebApi/InitialWebApi/Data/SessionsDbContext.cs
+++ WebApi/TherapEasy.WebApi/InitialWebApi/Data/SessionsDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace InitialWebApi.Data
+﻿using InitialWebApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace InitialWebApi.Data
 {
-    public class SessionsDbContext
+    public class SessionsDbContext : DbContext
     {
+        public DbSet<Sessions> Sessions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("YourConnectionStringHere");
+            }
+
+        }
     }
 }
